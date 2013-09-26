f2ddfdfd2eb57a8ea2d51fe01cbc1140f61a196c
Jesse Kipp and Colin MacKenzie <esbe.v2@gmail.com>
2013-09-26 15:40:00 -0400

    Fixes bugs in submit
    Prevents tag duplicates, clears tag labels after submission.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -11,12 +11,13 @@ class SecretsController < ApplicationController
   def create
     @secret = Secret.new(params[:secret])
     @secret.author_id = current_user.id
-    # @secret.recipient_id ||= params[:user_id]
+
+    @secret.tag_ids = params[:tag_ids].uniq
 
     if @secret.save
       respond_to do |format|
         format.html { redirect_to @secret.recipient }
-        format.json { render json: @secret }
+        format.json { render json: [@secret] + @secret.tags }
       end
     else
       redirect_to new_user_secret_url
